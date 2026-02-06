36a65524cc2c3d835b29343df5a37ca9c3500074
natan <ochenoficialniy@gmail.com>
2026-02-06 20:47:48 +0200

    first loader version

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,1 +1,44 @@
+const loader = document.getElementById('app-loader');
+
+function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('hidden');
+}
+
+const btn = document.getElementById('loadBtn');
+
+btn.addEventListener('click', () => {
+  showLoader();
+
+  setTimeout(() => {
+    hideLoader();
+  }, 2000);
+});
+
+window.addEventListener('load', () => {
+  hideLoader();
+});
+
+window.addEventListener('beforeunload', () => {
+  showLoader();
+});
+
+async function loadData() {
+  try {
+    showLoader();
+
+    const response = await fetch('https://sound-wave.b.goit.study/api');
+    const data = await response.json();
+
+    console.log(data);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    hideLoader();
+  }
+}
+
 console.log('loader');
